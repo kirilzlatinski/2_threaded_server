ab9776cdbec8ba27f608f64e517685f6a82c2ef8
kirilzlatinski <55870691+kirilzlatinski@users.noreply.github.com>
2019-11-01 14:34:12 +0300

    client

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
-import socket
-from time import sleep
+from socket import socket
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+sock = socket()
+host, port = input('Enter "host:port"\n').split(':')  # localhost:5050
 
-data = sock.recv(1024)
+try:
+    sock.connect((host, int(port)))
+except ConnectionError:
+    print("Connection error")
+else:
+    print(f"Connection with {host}:{port} established")
 
-sock.close()
+    try:
+        sock.send(input('Enter message to send: ').encode())
+    except ConnectionError:
+        print('Sending message error')
+    else:
+        print(f'Message sent to {host}:{port}')
 
-print(data.decode())
+        answer_blocks = []
+        while True:
+            answer_blocks.append(sock.recv(1024).decode())
+            if not answer_blocks[-1]:
+                break
+
+        answer = ''.join(answer_blocks)
+        print(f'{answer} gotten back from server')
+        sock.close()
+
+input('Press Enter to exit...')
